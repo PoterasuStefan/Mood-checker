f5879ae04c93d4e4b8cc137b06baacd5690025aa
stefan poterasu <spoterasu@gmail.com>
2023-04-16 14:17:21 +0300

    First commit

--- Mood checker/MainWindow.xaml.cs
+++ Mood checker/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Path = System.IO.Path;
 
 namespace Mood_checker
 {
@@ -20,9 +22,61 @@ namespace Mood_checker
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
+        int negative, positive, i, n, m, q, percent_positive, percent_negative ;
+        string[] negative_index = new string[60];
+        string[] positive_index = new string[60];
+        string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            string[] negative_index = {"sadness", "dejection", "depression", "despondency", "dismal", "feeling", "gloom", "gloominess", "grief", "heartache", "self - pity", "misery", "pain", "sorrow", "unhappiness", "woe","death", "sad", "depressing", "grievous", "heartbreaking", "plaintive", "mournful", "tragic"};
+            string[] positive_index = {"happiness", "bliss", "cheerfulness", "contentment", "delight", "elation", "euphoria", "gladness", "good feelings", "joy", "mirth", "pleasure", "satisfaction", "happy", "happily", "cheerful", "cheerfully", "chipper", "content"};
+            n = negative_index.Length;
+            m = positive_index.Length;
+
+        }
+
+        
+        
+
+
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.Enter))
+            {
+               for(i=1; i < n; i++)
+                {
+                    if (Input.Text.Contains(negative_index[i]))
+                    {
+                        negative++;
+                    }
+                    else if (Input.Text.Contains(positive_index[i]))
+                    {
+                        positive++; 
+                    }
+                }
+
+                q = 100 / (positive + negative);
+                percent_positive = q * positive;
+                percent_negative = q * negative;
+
+                if(percent_negative>percent_positive)
+                {
+                    //Emoji.Source = new System.Uri(baseDirectory + "\images\Sad-Face-Emoji.png");
+                    MessageBox.Show("Esti suparat");
+                    Accuracy_Label.Content=percent_negative + "%";
+                }
+                else
+                {
+                    MessageBox.Show("Esti fericit");
+                    Accuracy_Label.Content = percent_positive + "%";
+                }
+            }
         }
     }
 }
