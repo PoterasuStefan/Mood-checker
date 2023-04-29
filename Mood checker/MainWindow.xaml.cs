e71e202d73ec317bd7ee7d237ae0d60279f412d8
stefan poterasu <spoterasu@gmail.com>
2023-04-29 20:17:19 +0300

    Mood guesser v 1.0

--- Mood checker/MainWindow.xaml.cs
+++ Mood checker/MainWindow.xaml.cs
@@ -22,60 +22,81 @@ namespace Mood_checker
     /// </summary>
     public partial class MainWindow : Window
     {
+        int negative, positive, i, n, m, q, percent_positive, percent_negative;
+        string[] negative_index = new string[] { "sadness", "dejection", "depression", "despondency", "dismal", "feeling", "gloom", "gloominess", "grief", "heartache", "self-pity", "misery", "pain", "sorrow", "unhappiness", "woe", "death", "sad", "depressing", "grievous", "heartbreaking", "plaintive", "mournful", "tragic" };
+        string[] positive_index = new string[] { "happiness", "bliss", "cheerfulness", "contentment", "delight", "elation", "euphoria", "gladness", "good feelings", "joy", "mirth", "pleasure", "satisfaction", "happy", "happily", "cheerful", "cheerfully", "chipper", "content" };
 
 
-        int negative, positive, i, n, m, q, percent_positive, percent_negative ;
-        string[] negative_index = new string[60];
-        string[] positive_index = new string[60];
         string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            string[] negative_index = {"sadness", "dejection", "depression", "despondency", "dismal", "feeling", "gloom", "gloominess", "grief", "heartache", "self - pity", "misery", "pain", "sorrow", "unhappiness", "woe","death", "sad", "depressing", "grievous", "heartbreaking", "plaintive", "mournful", "tragic"};
-            string[] positive_index = {"happiness", "bliss", "cheerfulness", "contentment", "delight", "elation", "euphoria", "gladness", "good feelings", "joy", "mirth", "pleasure", "satisfaction", "happy", "happily", "cheerful", "cheerfully", "chipper", "content"};
             n = negative_index.Length;
             m = positive_index.Length;
 
         }
 
-        
-        
-
-
-
         private void TextBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (Keyboard.IsKeyDown(Key.Enter))
             {
-               for(i=1; i < n; i++)
+                negative = 0;
+                positive = 0;
+
+                for (i = 0; i < n; i++)
                 {
                     if (Input.Text.Contains(negative_index[i]))
                     {
                         negative++;
                     }
-                    else if (Input.Text.Contains(positive_index[i]))
-                    {
-                        positive++; 
-                    }
                 }
 
-                q = 100 / (positive + negative);
-                percent_positive = q * positive;
-                percent_negative = q * negative;
-
-                if(percent_negative>percent_positive)
+                for (i = 0; i < m; i++)
                 {
-                    //Emoji.Source = new System.Uri(baseDirectory + "\images\Sad-Face-Emoji.png");
-                    MessageBox.Show("Esti suparat");
-                    Accuracy_Label.Content=percent_negative + "%";
+                    if (Input.Text.Contains(positive_index[i]))
+                    {
+                        positive++;
+                    }
+                }
+                if (positive + negative > 0)
+                {
+                    q = 100 / (positive + negative);
+                    percent_positive = q * positive;
+                    percent_negative = q * negative;
+
+
+                    if (percent_negative > percent_positive)
+                    {
+
+                        Accuracy_Label.Content = percent_negative + "%";
+                        Emoji.Source = new BitmapImage(new Uri("/Mood checker;component/images/Sad-Face-Emoji.png", UriKind.Relative));
+
+
+                        MessageBox.Show("You are sad");
+
+                    }
+                    else if (percent_negative < percent_positive)
+                    {
+
+                        Accuracy_Label.Content = percent_positive + "%";
+                        Emoji.Source = new BitmapImage(new Uri("/Mood checker;component/images/happyface.jpg", UriKind.Relative));
+                        MessageBox.Show("You are happy");
+
+                    }
+                    else
+                    {
+                        Accuracy_Label.Content = "100%";
+                        Emoji.Source = new BitmapImage(new Uri("/Mood checker;component/images/Shrug.jpg", UriKind.Relative));
+                        MessageBox.Show("You are just as happy as you are sad 🤷 ");
+                    }
                 }
                 else
                 {
-                    MessageBox.Show("Esti fericit");
-                    Accuracy_Label.Content = percent_positive + "%";
+                    MessageBox.Show("I'm not sure if you are happy or sad");
                 }
+
             }
         }
     }
